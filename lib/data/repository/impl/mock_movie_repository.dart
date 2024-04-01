70b3593ff218ee914cb33a3790b3eb35edaa36a2
antran <trannhatan2803@gmail.com>
2024-04-01 09:14:39 +0700

    - mark visibleForTesting for mock_movie_repository.dart

--- lib/data/repository/impl/mock_movie_repository.dart
+++ lib/data/repository/impl/mock_movie_repository.dart
@@ -1,8 +1,10 @@
+import 'package:flutter/foundation.dart';
 import 'package:injectable/injectable.dart';
 import 'package:mockito/mockito.dart';
 import 'package:movie_app/data/datasource/base/i_movie_datasource.dart';
 import 'package:movie_app/data/repository/base/i_movie_repository.dart';
 import 'package:movie_app/domain/model/movie.dart';
 
+@visibleForTesting
 @injectable
 class MockMovieRepository extends Mock implements IMovieRepository {}
